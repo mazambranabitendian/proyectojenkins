ff292a58ab8a10efda59110c76047d59c926f580
mazambranabitendian <mazambrana@bitendian.com>
2012-12-18 11:20:42 +0100

    new file:   home.php

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+	public function holaMundo ()
+	{
+		echo "Hola Mundo!!";
+	}
+
+	public function adiosMundo ()
+	{
+		echo "Adios muy buenas!!";
+	}
+
+	public function sumaEnteros ( intA , intB )
+	{
+		return intA + intB;
+	}
+
+
+?>
